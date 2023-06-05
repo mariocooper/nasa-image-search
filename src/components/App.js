bdd96fba161977304e8f5c95fb3c6a3e577ff5ed
Mario Cooper <mariocooper00@hotmail.com>
2023-06-05 01:58:02 +0100

    conditional rendering added

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Search from "./Search";
 import SearchResults from "./SearchResults";
 
 function App() {
-  const [searchResults, setSearchResults] = useState();
+  const [searchResults, setSearchResults] = useState([]);
   return (
     <div className="app">
       <img 
@@ -13,7 +13,7 @@ function App() {
         alt="nasaLogo">
       </img>
       <Search setSearchResults={setSearchResults} />
-      <SearchResults />
+      <SearchResults results={searchResults} />
     </div>
   );
 };
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,17 +1,20 @@
 import React from "react";
 import "../styles/searchResults.css";
 
-function SearchResults() {
+function SearchResults({ results }) {
+    if (!results.length) {
+        return <p>No results</p>
+      } else {
         return (
           <div className="search-results">
-            <p>Search Results</p>
-              <img
+            <img
                 className="moon-image" 
                 src="https://images.unsplash.com/photo-1522030299830-16b8d3d049fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
                 alt="moon">
-              </img>
+            </img>
           </div>
         );
+    };
 };
 
 export default SearchResults;
