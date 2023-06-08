3d8a0275f3c31807e9df3212a746a24907ec83ed
Mario Cooper <mariocooper00@hotmail.com>
2023-06-08 18:52:17 +0100

    styling added - app complete

--- src/__tests__/Search.test.js
+++ src/__tests__/Search.test.js
@@ -1,16 +1,27 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Search from "../components/Search";
+import "@testing-library/jest-dom"
 
 describe("Search", () => {
+    const validProps = {
+        onChange: jest.fn(),
+        setValue: "moon",
+        setSearchResults: jest.fn(),
+        onSubmit: jest.fn()
+    }
     const { asFragment } = render(<Search />);
 
     it("renders correctly", () => {
-        expect(asFragment()).toMatchSnapshot();
-        });
+        expect(asFragment()).toMatchSnapshot();        });
 
     it("renders a search box", () => {
         render(< Search />);
         expect(screen.getByRole("textbox")).toBeInTheDocument();
-        });
+    });
+
+    it("renders a search button", () => {
+        render(< Search />);
+        expect(screen.getByRole("button")).toBeInTheDocument();
+    });
 });
